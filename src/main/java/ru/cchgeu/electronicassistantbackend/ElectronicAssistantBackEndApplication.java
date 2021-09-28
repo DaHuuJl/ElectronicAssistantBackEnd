a1f335746ff56812cff3ca3ec3af28bb77076e60
venar <venart-2@yandex.ru>
2021-09-28 23:48:06 +0300

    Initial commit

---
+++ src/main/java/ru/cchgeu/electronicassistantbackend/ElectronicAssistantBackEndApplication.java
@@ -0,0 +1,13 @@
+package ru.cchgeu.electronicassistantbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicAssistantBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicAssistantBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/cchgeu/electronicassistantbackend/ElectronicAssistantBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cchgeu.electronicassistantbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicAssistantBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
