3e0925d4ff5d5c43e9a37a736f189755203af5e5
Danyil Smirnov <venart-2@yandex.ru>
2022-03-18 01:18:26 +0300

    Добавил MailSender и тестовый контроллер для него, чтобы проверить отправляются ли сообщения. Дабавил TemplateEngine для создания шаблонов с джава кодом. Пример: Пользователь зарегался и нужно отправить приветственное сообщение Создаём html шаблон в static/templates с помощью таймлиф, затем в TemplateEngineServiceImpl добавляем метод который будет вызывать этот шаблон и пихать туда данные, затем получаем шаблон с этими данным и отправляем с поиощью MailSender

--- src/main/java/ru/cchgeu/electronicassistantbackend/controllers/MailController.java
+++ src/main/java/ru/cchgeu/electronicassistantbackend/controllers/MailController.java
@@ -7,20 +7,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.cchgeu.electronicassistantbackend.model.dto.MailMassage;
-import ru.cchgeu.electronicassistantbackend.services.impl.MailSenderServiceImpl;
+import ru.cchgeu.electronicassistantbackend.services.impl.mail.MailSenderServiceImpl;
+import ru.cchgeu.electronicassistantbackend.services.impl.mail.TemplateEngineServiceImpl;
 
+/*
+    TODO: Этот контроллер создан, чтобы проверить работает ли отправка сообщений, его нужно убрать в будущем
+*/
 @RestController@RequestMapping(value = "/api/mail/sender",produces = {MediaType.APPLICATION_JSON_VALUE})
 public class MailController {
 
     private final MailSenderServiceImpl mailSenderService;
+    private final TemplateEngineServiceImpl templateEngineService;
 
     @Autowired
-    public MailController(MailSenderServiceImpl mailSenderService) {
+    public MailController(MailSenderServiceImpl mailSenderService, TemplateEngineServiceImpl templateEngineService) {
         this.mailSenderService = mailSenderService;
+        this.templateEngineService = templateEngineService;
     }
 
     @PostMapping(path = "/send")
     public void send(@RequestBody MailMassage massage) {
-        mailSenderService.send(massage.getEmail(), massage.getSubject(), massage.getMessage());
+        mailSenderService.sendMessage(massage.getEmail(), massage.getSubject(), massage.getMessage());
+    }
+
+    @PostMapping(path = "/sendDocument")
+    public void sendDocument(@RequestBody MailMassage massage) {
+        mailSenderService.sendHtmlDocument(massage.getEmail(), massage.getSubject(), templateEngineService.getTestDocument(massage.getEmail(), massage.getSubject()));
     }
 }
--- src/main/java/ru/cchgeu/electronicassistantbackend/services/MailSenderService.java
+++
@@ -1,6 +0,0 @@
-package ru.cchgeu.electronicassistantbackend.services;
-
-public interface MailSenderService {
-
-    public void send(String email, String subject, String message);
-}
